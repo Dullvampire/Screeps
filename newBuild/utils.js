28f71ccb9b37fb4ca07b7a3efaa1be91e4c1e54d
Dullvampire <danterella@rocketmail.com>
2017-02-01 13:26:15 -0800

    No longer serializes path
    I dont know why it doesnt work, it just doesnt

--- newBuild/utils.js
+++ newBuild/utils.js
@@ -51,7 +51,7 @@ module.exports = {
                     range = 1;
                 }
 
-                creep.memory.pathToFollow = {goalPos: goal, path: Room.serializePath(this.getPathTo(creep.pos, {pos: goal, range: range}))};
+                creep.memory.pathToFollow = {goalPos: goal, path: this.getPathTo(creep.pos, {pos: goal, range: range})};
             }
             if (creep.memory.pathToFollow) {
                 creep.moveByPath(creep.memory.pathToFollow.path);
